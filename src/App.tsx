7a9fb7f98e7d95a4e394a549fd0b7a9e3bc1c684
Miguel <miguel_gonzales@dlsu.edu.ph>
2025-01-25 02:04:14 +0800

    fix: fixed main page

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 function App() {
   return (
     <div>
-      <h1 className='text-blue-500 underline text-3xl font-bold italic'>Hello, world!</h1>
+      <form action="">
+        <h1></h1>
+        <div></div>
+        <div></div>
+        <div></div>
+      </form>
     </div>
   )
 }
