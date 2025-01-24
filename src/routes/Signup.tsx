dcf0a0adea1a175dbc87d90ca7c98dbf0f0eac89
Miguel <miguel_gonzales@dlsu.edu.ph>
2025-01-25 02:50:14 +0800

    feat: started on signup form styling

--- src/routes/Signup.tsx
+++ src/routes/Signup.tsx
@@ -9,16 +9,16 @@ TODO: Signup form should accomodate the following fields:
 
 function Signup() {
   return (
-    <div>
-      <form action="submit">
-        <h1>Signup</h1>
-        <div>
-          <label htmlFor="fname"></label>
+    <div className="h-screen w-screen flex items-center justify-center">
+      <form action="submit" className="h-2/3 w-1/5 bg-blue-300 rounded-3xl p-6 flex flex-col items-center">
+        <h1 className="text-white text-6xl font-bold">Signup</h1>
+        <div className="w-full flex flex-col">
+          <label htmlFor="fname" className="text-2xl">First Name</label>
           <input type="text" name="fname"/>
         </div>
 
-        <div>
-          <label htmlFor="email"></label>
+        <div className="w-full flex flex-col">
+          <label htmlFor="email" className="text-2xl">Email</label>
           <input type="text" name="email"/>
         </div>
 
